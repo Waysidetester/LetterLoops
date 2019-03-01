ba945f80ba52766c18f9b00f145785d8a45f96bd
WaysideTester <dylanmurray211@outlook.com>
2019-02-28 22:10:49 -0600

    better comments

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -16,9 +16,14 @@ namespace LetterLoops
             // -------- Loop over user input -----------
             for (int i = 0; i < userInput.Length; i++)
             {
+                // Allows the ToUpper() method. Code wouldn't allow it on char
                 string charToString = Convert.ToString(userInput[i]);
                 string upperString = charToString.ToUpper();
+
+                // Adds upper case char to the final output
                 finalOutput += upperString;
+
+                // checks position to determine if additional lowercase chars need to be added
                 if (i > 0)
                 {
                     for (int j = 0; j < i; j++)
@@ -26,12 +31,15 @@ namespace LetterLoops
                         finalOutput += userInput[i];
                     }
                 }
+
+                // adds "-" after each char except for final char in string
                 if (i != (userInput.Length - 1))
                 {
-                finalOutput += "-";
+                    finalOutput += "-";
                 }
-                Console.WriteLine($"{userInput[i]} index is {i} and uppercase is {upperString}");
             }
+
+            // prints final output
             Console.WriteLine(finalOutput);
             Console.WriteLine("press ENTER to exit");
             Console.ReadLine();
