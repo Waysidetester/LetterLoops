4a4f70e77fdc23590138e758871a3ab1f82d442b
WaysideTester <dylanmurray211@outlook.com>
2019-02-28 21:52:22 -0600

    iterating over strings as per instructions.

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -6,7 +6,35 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // --------- Init app ----------------------
+            Console.WriteLine("Please input a string");
+            
+            // ------ Accept/ Set User Input -----------
+            string userInput = Console.ReadLine();
+            string finalOutput = "";
+
+            // -------- Loop over user input -----------
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                string charToString = Convert.ToString(userInput[i]);
+                string upperString = charToString.ToUpper();
+                finalOutput += upperString;
+                if (i > 0)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        finalOutput += userInput[i];
+                    }
+                }
+                if (i != (userInput.Length - 1))
+                {
+                finalOutput += "-";
+                }
+                Console.WriteLine($"{userInput[i]} index is {i} and uppercase is {upperString}");
+            }
+            Console.WriteLine(finalOutput);
+            Console.WriteLine("press ENTER to exit");
+            Console.ReadLine();
         }
     }
 }
